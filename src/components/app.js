cc980285d5ef0a4be25fd0c09ea4a3ea92c3cb6f
Stephen Mayeux <stephenmayeux@gmail.com>
2016-10-17 22:49:45 -0400

    style body. add headers

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1 className="site-title">
+          <a href="/">quizBuzz Presents</a>
+        </h1>
+        <h2 className="site-title-dark">
+          <a href="/">Which 90s Hip-Hop Artist Is Your Spirit Animal?</a>
+        </h2>
+      </div>
     );
   }
 }
