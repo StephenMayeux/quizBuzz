78ace0f2478f12ae70b3c907eabadcc6e46cec0b
Stephen Mayeux <stephenmayeux@gmail.com>
2016-10-17 21:56:10 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Require modules
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+// App set up
+app.use(express.static(__dirname));
+
+// All requests
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+// Spin it up on 
+app.listen(port);
+console.log('Server started');
